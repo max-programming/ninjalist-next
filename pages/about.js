d7c16826cabd343c9998bbb4b3806ec92916686e
max-programming <ui.sabuwala@gmail.com>
2021-02-05 19:21:02 +0530

    First commit

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla earum
+          voluptas iste neque, qui veritatis ea nam enim. Laudantium voluptatum,
+          molestiae voluptas aspernatur in odio libero explicabo assumenda minus
+          corporis.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla earum
+          voluptas iste neque, qui veritatis ea nam enim. Laudantium voluptatum,
+          molestiae voluptas aspernatur in odio libero explicabo assumenda minus
+          corporis.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
